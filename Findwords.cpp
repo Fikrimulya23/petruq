dfdf75b666f55f9805dbdf24c1b80f9953f87909
arsyisobirin <48671439+arsyisobirin@users.noreply.github.com>
2019-03-20 20:51:59 +0700

    Update Findwords.cpp

--- Findwords.cpp
+++ Findwords.cpp
@@ -12,21 +12,21 @@ void awal () {
 	cout<<"====================================\n\n";
 }
 
-char s0[15]	= 	{'T','G','B','W','W','I','N','T','E','R','W','S','E','S','N'};
-char s1[15]	=	{'A','A','U','N','T','T','M','M','H','F','O','O','D','N','B'};
-char s2[15]	=	{'J','L','W','C','Q','L','D','Z','M','P','M','V','D','M','R'};
-char s3[15]	=	{'A','S','A','G','M','Q','U','W','V','V','B','S','O','H','I'};
-char s4[15]	=	{'B','W','P','L','O','T','A','N','A','D','T','P','G','N','C'};
-char s5[15]	=	{'R','E','W','N','G','O','D','J','C','P','N','A','T','N','K'};
-char s6[15]	=	{'E','E','O','T','W','O','S','B','Q','H','A','R','R','S','A'};
-char s7[15]	=	{'A','Z','C','G','E','S','W','E','W','N','A','K','N','P','B'};
-char s8[15]	=	{'D','I','N','N','E','R','Q','O','D','L','W','D','C','A','R'};
-char s9[15]	=	{'O','N','O','P','K','W','M','P','A','R','K','T','Z','C','C'};
-char s10[15]	=	{'Q','B','F','R','O','G','M','A','M','W','P','W','E','E','Y'};
-char s11[15]	=	{'L','Q','Z','Q','N','N','M','R','Z','J','J','S','C','L','G'};
-char s12[15]	=	{'M','O','S','G','Z','C','Z','E','T','D','B','O','O','T','O'};
-char s13[15]	=	{'P','D','C','R','Z','M','S','N','G','R','D','N','R','P','Z'};
-char s14[15]	= 	{'O','H','N','K','Z','W','A','T','E','R','J','G','T','R','A'};
+char s0[15]	 = 	{'T','G','B','W','W','I','N','T','E','R','W','S','E','S','N'};
+char s1[15]	 =	{'A','A','U','N','T','T','M','M','H','F','O','O','D','N','B'};
+char s2[15]	 =	{'J','L','W','C','Q','L','D','Z','M','P','M','V','D','M','R'};
+char s3[15]	 =	{'A','S','A','G','M','Q','U','W','V','V','B','S','O','H','I'};
+char s4[15]	 =	{'B','W','P','L','O','T','A','N','A','D','T','P','G','N','C'};
+char s5[15]	 =	{'R','E','W','N','G','O','D','J','C','P','N','A','T','N','K'};
+char s6[15]	 =	{'E','E','O','T','W','O','S','B','Q','H','A','R','R','S','A'};
+char s7[15]	 =	{'A','Z','C','G','E','S','W','E','W','N','A','K','N','P','B'};
+char s8[15]	 =	{'D','I','N','N','E','R','Q','O','D','L','W','D','C','A','R'};
+char s9[15]	 =	{'O','N','O','P','K','W','M','P','A','R','K','T','Z','C','C'};
+char s10[15]	 =	{'Q','B','F','R','O','G','M','A','M','W','P','W','E','E','Y'};
+char s11[15]	 =	{'L','Q','Z','Q','N','N','M','R','Z','J','J','S','C','L','G'};
+char s12[15]	 =	{'M','O','S','G','Z','C','Z','E','T','D','B','O','O','T','O'};
+char s13[15]	 =	{'P','D','C','R','Z','M','S','N','G','R','D','N','R','P','Z'};
+char s14[15]	 = 	{'O','H','N','K','Z','W','A','T','E','R','J','G','T','R','A'};
 char *(soal[15]) = {s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14};
 
 //Pemanggil puzzle
@@ -40,7 +40,6 @@ void panggil_soal(){
 }
 
 //Ubah semua huruf menjadi huruf kapital
-
 void menaik( char *word){
 	int x = 'A' - 'a';
 	if (x < 0) {
@@ -56,7 +55,6 @@ void menaik( char *word){
 }
 
 //menghitung panjang array
-
 int panjang(char *word){
 	int x;
 	for (int i=0;*(word+i);i++){
@@ -124,8 +122,9 @@ int main(){
 	awal();
 	panggil_soal();	
 	int jumlah_kata;
-	cout << "\nBerapa banyak kata yang akan dicari: ";
+	cout << "\nBerapa banyak kata yang ingin dicari: ";
 	cin >> jumlah_kata;
+	cout << "\n";
 	char word[jumlah_kata][15];
 
 	for (int i=0;i<jumlah_kata;i++){
